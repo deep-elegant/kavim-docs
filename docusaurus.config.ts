538ebb6b4396847eb8045619e868ecdc3c925b73
Shahar Shalev <shaharshalev0@gmail.com>
2025-10-23 12:25:45 +0300

    fix: more cleanup

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -34,18 +34,19 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   presets: [
     [
       'classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -64,14 +65,9 @@ const config: Config = {
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
+        target: 'docs/intro',
       },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
         {
           href: 'https://discord.gg/xTFEu5Q6q6',
           label: 'Discord',
